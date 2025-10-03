0d4669c7250e85efd1ef74018964521ead5cdabe
Jeffrey Husi <1x2white+git@gmail.com>
2025-10-04 00:10:37 +0200

    Deployed a811748 with MkDocs version: 1.6.1

---
+++ Tag4.d
@@ -0,0 +1,10 @@
+## Skansen
+Heute war ich wieder auf der gleichen Insel wie gestern, da ich noch zwei Sachen hatte, die ich besuchen wollte. Das eine war das Skansen. Dies ist ein relativ grosses Gebiet, worin sich einerseits ein Tierpark andererseits eine Sammlung historischer Gebäude befindet. Der Eintritt kostet rund 19 CHF. 
+
+![Historische Gebäude](img/day4/img_001.jpeg)
+
+Auch hier gibt es die Möglichkeit, einem Audio Guide mitsamt Karte zu folgen, den man auf der Webseite des Parks findet. Weiter gibt es die Aufteilung zwischen historischer Route der Tier Route66 oder der Kombination von beidem. Ich habe mich dazu entschieden, der langen Route zu folgen. Die Führung fängt in einem kleinen Dorf an, wo man verschiedene alte Handwerker betrachten kann wie zum Beispiel den Töpfer oder den Glasbläser.
+Aber auch abseits der Route, die Audioguide vorschreibt, gibt es vieles zu entdecken.
+
+
+## Nordiska Museet
